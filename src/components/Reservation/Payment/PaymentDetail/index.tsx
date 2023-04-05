82fb55a8b663054e5b2dfe2d613feba5a383c2f0
vinhnguyenc2c <vinh.nguyen@c2c-techhub.io>
2023-04-05 16:54:17 +0700

    fix: MAN-421 Fix view payment confirmation screen when change menu

--- src/components/Reservation/Payment/PaymentDetail/index.tsx
+++ src/components/Reservation/Payment/PaymentDetail/index.tsx
@@ -3,27 +3,22 @@ import CommonSection from 'components/CommonSection';
 import ContentLine from 'components/Reservation/ReservationDetail/ReservationContent/ContentLine';
 import type { TreatmentFormValues } from 'components/Reservation/ReservationTreatment/models/schema';
 import dayjs from 'dayjs';
+import type { IMenu } from 'models/menu/interface';
 import type { ISalonInfo, ResultMenu } from 'models/reservation/interface';
 import React from 'react';
 import { NumericFormat } from 'react-number-format';
 import { DATE_FORMAT } from 'utils/const';
-import queryClient from 'utils/queryClient';
 
 import PaymentFee from '../PaymentFee';
 import TreatmentDetail from '../TreatmentDetail';
 import styles from './styles';
-
-interface IMenuList {
-  id: string | number;
-  name: string | number;
-}
 
 interface IPaymentDetail extends ResultMenu {
   startTime: string;
   endTime: string;
   salonInfo: ISalonInfo;
   initialTreatmentValues: TreatmentFormValues;
-  menuUpdatingInfo: IMenuList[];
+  menuUpdatingInfo: IMenu[];
 }
 
 const PaymentDetail: React.FC<IPaymentDetail> = ({
@@ -34,21 +29,18 @@ const PaymentDetail: React.FC<IPaymentDetail> = ({
   initialTreatmentValues,
   menuUpdatingInfo,
 }) => {
-  const valueDate =
-    startTime || endTime ? dayjs(startTime || endTime) : dayjs();
-  const queryMenuList =
-    queryClient
-      .getQueryCache()
-      .findAll(['menu', 'list', 'salonId'])
-      .map((each) => {
-        return each?.state?.data;
-      }) || [];
+  const handleGetDate = () => {
+    const valueDate =
+      startTime || endTime ? dayjs(startTime || endTime) : dayjs();
+    return valueDate.format(DATE_FORMAT);
+  };
 
-  const menuListData: any = queryMenuList?.[0];
-  const currentList = menuListData?.docs || [];
-  const currentMenu = currentList.filter(
-    (item: any) => item._id === initialTreatmentValues?.menuId,
-  );
+  const handleGetEndTime = () => {
+    const endTimeMili = (menuUpdatingInfo?.[0]?.estimatedTime || 1) * 60000;
+    const newEndTime = dayjs.utc(startTime).valueOf() + endTimeMili;
+
+    return newEndTime;
+  };
 
   return (
     <CommonSection title="予約内容">
@@ -57,15 +49,20 @@ const PaymentDetail: React.FC<IPaymentDetail> = ({
           <ContentLine start="整体師" center={salonInfo?.name} />
           <ContentLine
             start="ご予約日"
-            center={`${valueDate.format(DATE_FORMAT)}(月) ${dayjs
+            center={`${handleGetDate()}(月) ${dayjs
               .utc(startTime)
               .tz()
-              .format('HH:mm')} ~ ${dayjs.utc(endTime).tz().format('HH:mm')}`}
+              .format('HH:mm')} ~ ${dayjs
+              .utc(handleGetEndTime())
+              .tz()
+              .format('HH:mm')}`}
           />
           <ContentLine
             start="予約コース"
             center={`${menuUpdatingInfo?.[0]?.name || menuInfo?.name} ${
-              currentMenu?.[0]?.estimatedTime || menuInfo?.estimatedTime || 0
+              menuUpdatingInfo?.[0]?.estimatedTime ||
+              menuInfo?.estimatedTime ||
+              0
             }分`}
             end={
               <NumericFormat
