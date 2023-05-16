fecc2bfed97b6149962bc1ea07336a05c1830307
CodeNinja0718 <coolfighter0505@gmail.com>
2023-05-16 15:18:17 +0900

    feat: MAN-518 Fix bug when back broswer

--- src/components/MenuForm/index.tsx
+++ src/components/MenuForm/index.tsx
@@ -1,41 +1,21 @@
 import { useUser } from 'hooks';
-import _map from 'lodash/map';
-import useInitValue from 'pages/my-page/menu/hook/useInitValue';
 import { useState } from 'react';
-import { CURRENCY } from 'utils/const';
-import queryClient from 'utils/queryClient';
 
 import Component from './component';
 import type { MenuFormValues } from './models/schema';
 
 interface MenuFormProps {
   onSubmit: (param: MenuFormValues) => void;
-  defaultValues?: MenuFormValues;
+  defaultValues: MenuFormValues;
 }
 
 const MenuForm = ({ onSubmit, defaultValues }: MenuFormProps) => {
   const [loading, setLoading] = useState(false);
-  const initValues = { ...useInitValue(), ...defaultValues };
-
   useUser({ enabled: false });
 
-  const currentUserQuery = queryClient
-    .getQueryCache()
-    .findAll(['currentUser'])
-    .map((each) => {
-      return each?.state?.data;
-    });
-
   const handleSubmit = (value: MenuFormValues) => {
-    const manipulatorIds = _map(currentUserQuery, (item: any) => item._id);
-    const params = {
-      ...value,
-      manipulatorIds,
-      currency: CURRENCY.JPY,
-    };
-
     setLoading(true);
-    onSubmit(params);
+    onSubmit(value);
   };
 
   return (
@@ -43,7 +23,7 @@ const MenuForm = ({ onSubmit, defaultValues }: MenuFormProps) => {
       loading={loading}
       onSubmit={handleSubmit}
       initialValues={{
-        ...initValues,
+        ...defaultValues,
       }}
     />
   );
