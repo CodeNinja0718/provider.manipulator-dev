1ca8d774639b23601b3659f7ce34f42861f5a422
vinhnguyenc2c <vinh.nguyen@c2c-techhub.io>
2023-06-01 15:31:57 +0700

    fix: MAN-000 Fix build

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -10,7 +10,6 @@ import type { ToastContent, ToastOptions } from 'react-toastify';
 import { toast } from 'react-toastify';
 
 import { DATE_FORMAT, DOCUMENT_TYPES, FILE_TYPES } from './const';
-import queryClient from './queryClient';
 
 const Helper = {
   getWebCookie: (
@@ -181,124 +180,6 @@ const Helper = {
       return currentDate.format(DATE_FORMAT);
     }
     return dayjs().format(DATE_FORMAT);
-  },
-
-  convertManipulatorProfile: (detailData: IManipulatorItem) => {
-    const {
-      name,
-      nameKana,
-      email,
-      photos,
-      defaultShifts,
-      careerStart,
-      profile,
-      pr,
-      supportedSymptoms,
-      nationalLicenses,
-      verifyEmail,
-    } = detailData;
-
-    const avatarContent = photos.find((photo) => photo.type === 'avatar');
-    const photoContent = photos.filter((photo) => photo.type === 'pr');
-    let avatar = null;
-    const photoImages: any[] = [];
-
-    if (avatarContent) {
-      avatar = {
-        url: avatarContent.url,
-        fileUrl: avatarContent.url,
-        originUrl: avatarContent.url,
-        objectKey: avatarContent.objectKey,
-        key: avatarContent.objectKey,
-      };
-    }
-
-    if (photoContent.length > 0) {
-      photoContent.forEach((photo) => {
-        photoImages.push({
-          url: photo.url,
-          fileUrl: photo.url,
-          originUrl: photo.url,
-          objectKey: photo.objectKey,
-          key: photo.objectKey,
-        });
-      });
-    }
-
-    const newValues: ManipulatorProfileValues = {
-      name,
-      nameKana,
-      email,
-      pr,
-      avatar,
-      engagement: Number(careerStart) || 0,
-      symptoms: supportedSymptoms.map((item) => item.id),
-      description: profile,
-      qualification: nationalLicenses.map(
-        (item) => QUALIFICATION.find((value) => value.name === item)?.id,
-      ),
-      businessHours: defaultShifts.map((item) => ({
-        ...item,
-        hours: item.hours.map((hour) => ({
-          startTime: dayjs(hour.startTime).tz().format('HH:mm'),
-          endTime: dayjs(hour.endTime).tz().format('HH:mm'),
-        })),
-      })),
-      photos: photoImages,
-      isRegister: verifyEmail ? ['confirm_register'] : [],
-    };
-
-    return newValues;
-  },
-
-  convertManipulatorProfileUpdate: (
-    profileData: ManipulatorProfileValues,
-    manipulatorId: string,
-  ) => {
-    const {
-      avatar,
-      symptoms,
-      description,
-      qualification,
-      photos,
-      businessHours,
-      engagement,
-      isRegister,
-      ...rest
-    } = profileData;
-
-    const photoArray =
-      photos?.map((photo) => ({
-        type: 'pr',
-        objectKey: photo.key,
-      })) || [];
-
-    if (avatar) {
-      photoArray.push({
-        type: 'avatar',
-        objectKey: avatar.key,
-      });
-    }
-
-    return {
-      ...rest,
-      careerStart: `${engagement}`,
-      profile: description,
-      supportedSymptoms: symptoms,
-      nationalLicenses: qualification?.map(
-        (item) => QUALIFICATION.find((value) => value.id === item)?.name,
-      ),
-      photos: photoArray,
-      defaultShifts:
-        businessHours?.map((businessHour) => ({
-          ...businessHour,
-          hours: businessHour.hours?.filter(
-            ({ startTime, endTime }) => startTime && endTime,
-          ),
-        })) || [],
-      verifyEmail: (isRegister?.length || 0) > 0,
-      manipulatorId,
-    };
   },
 };
 
