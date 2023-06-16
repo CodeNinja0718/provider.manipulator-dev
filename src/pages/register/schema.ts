b93bc1e447677205f13af3bfb950125ecae465f3
Tri Vu <tri.vu@c2c-techhub.io>
2023-06-16 16:24:39 +0700

    chore: MAN-602 Update invalid email error text

--- src/pages/register/schema.ts
+++ src/pages/register/schema.ts
@@ -3,7 +3,9 @@ import type { InferType } from 'yup';
 import { object, string } from 'yup';
 
 const schema = object({
-  email: string().required().matches(Regex.EMAIL, 'Email is not valid'),
+  email: string()
+    .required()
+    .matches(Regex.EMAIL, '正しい形式のメールアドレスを入力してください。'),
 });
 
 export default schema;
